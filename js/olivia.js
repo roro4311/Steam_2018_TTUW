e49916cfdbf266dfb91dfd387332c5c775b8cfef
Sherill Samuel <sherill.samuel@gmail.com>
2018-08-09 03:05:52 -0400

    first commit

---
+++ js/olivia.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function() {
+  $('#trigger').click(function() {
+    $('#overlay').fadeIn(300);  
+  });
+  $('#close').click(function() {
+    $('#overlay').fadeOut(300);
+  });
+});
